23fb61e6e16e6b95cdf1573025c574bf82640e88
hagattavalias-creator <hagatta.valias@escola.pr.gov.br>
2026-06-12 08:38:52 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem(){
+    document.getElementById("mensagem").innerHTML =
+    "🌱 Usar menos veneno na plantação ajuda a produzir alimentos mais saudáveis, protege a natureza e garante um futuro melhor para todos!";
+}
